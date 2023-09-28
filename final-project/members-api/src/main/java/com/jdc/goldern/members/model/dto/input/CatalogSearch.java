afb1d58d1bcd6f58f6405f3144139bf43a39641a
Min Lwin <lwin.zawmin@gmail.com>
2023-09-28 20:24:28 +0630

    Update Catalog Search

--- final-project/members-api/src/main/java/com/jdc/goldern/members/model/dto/input/CatalogSearch.java
+++ final-project/members-api/src/main/java/com/jdc/goldern/members/model/dto/input/CatalogSearch.java
@@ -1,5 +1,6 @@
 package com.jdc.goldern.members.model.dto.input;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.domain.Specification;
@@ -7,6 +8,7 @@ import org.springframework.util.StringUtils;
 
 import com.jdc.goldern.members.model.entity.Catalog;
 
+import jakarta.persistence.criteria.JoinType;
 import lombok.Data;
 
 @Data
@@ -14,18 +16,22 @@ public class CatalogSearch {
 
 	private Optional<String> keyword;
 
-	private Optional<Integer> categoryId;
+	private Optional<List<Integer>> categoryId;
 
 	public Specification<Catalog> where() {
 		Specification<Catalog> spec = Specification.where(null);
 		
-		spec = null == categoryId || categoryId.filter(id -> id > 0).isEmpty() ? spec :
-			spec.and((root, query, cb) -> cb.equal(root.get("category").get("id"), categoryId.get()));
+		spec = null == categoryId || categoryId.filter(ids -> !ids.isEmpty()).isEmpty() ? spec :
+			spec.and((root, query, cb) -> {
+				var inClause = cb.in(root.join("categories", JoinType.LEFT).get("id"));
+				categoryId.get().forEach(inClause::value);
+				return inClause;
+			});
 
 		spec = null == keyword || keyword.filter(StringUtils::hasLength).isEmpty() ? spec :
 			spec.and((root, query, cb) -> cb.or(
 				cb.like(cb.lower(root.get("name")), keyword.get().toLowerCase().concat("%")),
-				cb.like(cb.lower(root.get("catagory").get("name")), keyword.get().toLowerCase().concat("%"))
+				cb.like(cb.lower(root.join("categories", JoinType.LEFT).get("name")), keyword.get().toLowerCase().concat("%"))
 			));
 		
 		return spec;
